134dc34bac35c7924ce93c75573f2f884e4b4f8e
Nezumi55 <bkalk109@gmail.com>
2023-05-20 23:31:59 +0900

    auto logout

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   View,
@@ -17,13 +16,19 @@ export default function LogInScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
 
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'MemoList' }],
+        });
+      }
     });
-  });
-  
+    return unsubscribe;
+  }, []);
+
   function handlePress() {
     firebase
       .auth()
